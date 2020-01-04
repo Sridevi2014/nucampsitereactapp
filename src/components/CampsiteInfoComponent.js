36ddc274609dd222fb889b7af316541ef18ddf5b
Sridevi2014 <sridevips@hotmail.com>
2020-01-04 12:32:00 -0600

    added <p> tags

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -29,7 +29,7 @@ class CampsiteInfoComponent extends Component {
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
                     {comments.map(comment => {return (
-                        <div key={comment.id}>{comment.text} <br /> -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</div>
+                        <div key={comment.id}>{comment.text}<p> -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p> </div>
                     )})}
                 </div>
             )
